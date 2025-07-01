d9394654b3089bd86cf3a6eeadc9e30088b0657a
Colgemelli <sistemas@colgemelli.edu.co>
2025-07-01 12:56:50 -0500

    env.js

---
+++ env.js
@@ -0,0 +1,1 @@
+window.process = { env: { SUPABASE_URL: "https://zkbnpjmtwkhcvqizpmhj.supabase.co", SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprYm5wam10d2toY3ZxaXpwbWhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTExNTQyNDksImV4cCI6MjA2NjczMDI0OX0.McMyTT8-Myp6L0nIjTN4chedAPunB0dwymQKhiNp6uI" } };
